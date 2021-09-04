010bd52f43a9197d6276663eac41ea40323e6c05
oleh_matsevych <olegmacevich@ukr.net>
2021-09-04 14:34:16 +0300

    Setup env

---
+++ OlehMatsevych.RobotChallange/OlehMatsevychAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using Robot.Common;
+using System;
+using System.Collections.Generic;
+
+namespace OlehMatsevych.RobotChallange
+{
+    public class OlehMatsevychAlgorithm : IRobotAlgorithm
+    {
+        public string Author { get { return "Oleh Matsevych"; } }
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+
+            var robot = robots[robotToMoveIndex];
+
+            var newPosition = robot.Position;
+            newPosition.X += 2;
+            newPosition.Y += 2;
+            return new MoveCommand() { NewPosition = newPosition };
+
+        }
+    }
+}
